d0dd2eee39005fc04144d197dc2826e25a6de55f
Kellana <kellyg15@uia.no>
2017-10-11 17:48:47 +0200

    Min første node aplikasjon

---
+++ greeter.js
@@ -0,0 +1,4 @@
+var greetings = ["Hello", "Hi", "Hey", "Hey I am the doctor", "Hei", "May the force be with you", "Look what the cat dragged in!", "Y-ellow", "Oh no, not you again!"];
+var randomIndex = Math.floor(Math.random()*greetings.length);
+
+console.log(greetings[randomIndex]);
